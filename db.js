55e1792512f97272f86454b71f6c96af109cceb5
Abid-Hussain13 <nowaymate13$>
2025-07-08 22:35:15 +0500

    edit env

--- db.js
+++ db.js
@@ -1,18 +1,36 @@
-import mysql from 'mysql';
+// import mysql from 'mysql';
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',        
-    password: '',        
-    database: 'users'
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',        
+//     password: '',        
+//     database: 'users'
+// });
+
+// db.connect((err) => {
+//     if (err) {
+//         console.error('Database connection failed:', err.stack);
+//         return;
+//     }
+//     console.log('Connected to MySQL database.');
+// });
+
+// export default db;
+
+
+/////////////////// Database Setup to Run of Railways.com ////////////////////////
+
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config()({Path: "./.env"});
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error('Database connection failed:', err.stack);
-        return;
-    }
-    console.log('Connected to MySQL database.');
-});
-
-export default db;
+module.exports = connection;
